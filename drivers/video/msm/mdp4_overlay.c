aa78e9b0f62b0d67b3e5c63565f2d8ece47487f9
Kuogee Hsieh <khsieh@codeaurora.org>
2013-04-15 16:04:54 -0700

    Revert "msm_fb: display: don't enable blt mode"
    This reverts commit aa980658ac73a347fc94108d4fc886860d0c1714 because
    of iommu page faults on some targets.
    
    Change-Id: I2b0f801ab00f473dcb3f3918b30ebf594a5a2058
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdp4_overlay.c

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -3177,9 +3177,6 @@ int mdp4_overlay_mdp_perf_req(struct msm_fb_data_type *mfd)
 		 perf_req->use_ov_blt[0],
 		 perf_req->use_ov_blt[1]);
 
-	if (perf_req->use_ov_blt[0] || perf_req->use_ov_blt[1])
-		return ret;
-
 	return 0;
 }
 
@@ -3434,7 +3431,7 @@ int mdp4_overlay_get(struct fb_info *info, struct mdp_overlay *req)
 int mdp4_overlay_set(struct fb_info *info, struct mdp_overlay *req)
 {
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
-	int ret = 0, mixer;
+	int ret, mixer;
 	struct mdp4_overlay_pipe *pipe;
 
 	if (mfd == NULL) {
@@ -3540,14 +3537,9 @@ int mdp4_overlay_set(struct fb_info *info, struct mdp_overlay *req)
 #endif
 #endif
 
-	ret = mdp4_overlay_mdp_perf_req(mfd);
-
-	if (ret)
-		pr_err("%s: blt mode should not be enabled\n", __func__);
-
 	mutex_unlock(&mfd->dma->ov_mutex);
 
-	return ret;
+	return 0;
 }
 
 int mdp4_overlay_unset_mixer(int mixer)
