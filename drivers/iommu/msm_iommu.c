e2159a0111817967af0fccc2012ed84eb7f00dbc
Jeremy Gebben <jgebben@codeaurora.org>
2013-04-19 13:45:03 -0600

    iommu: msm: prevent partial mappings on error
    If msm_iommu_map_range() fails mid way through the va
    range with an error, clean up the PTEs that have already
    been created so they are not leaked.
    
    Change-Id: Ie929343cd6e36cade7b2cc9b4b4408c3453e6b5f
    CRs-Fixed: 478304
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>
    
    Conflicts:
    	drivers/iommu/msm_iommu.c

--- drivers/iommu/msm_iommu.c
+++ drivers/iommu/msm_iommu.c
@@ -49,6 +49,8 @@ __asm__ __volatile__ (							\
 #define MSM_IOMMU_ATTR_CACHED_WB_NWA	0x2
 #define MSM_IOMMU_ATTR_CACHED_WT	0x3
 
+static int msm_iommu_unmap_range(struct iommu_domain *domain, unsigned int va,
+				 unsigned int len);
 
 static inline void clean_pte(unsigned long *start, unsigned long *end,
 			     int redirect)
