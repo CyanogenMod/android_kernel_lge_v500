MotorolaMobilityLLC/kernel-msm

msm: vidc: set EOS on output buffer pending transaction
If EOS is signalled while an output buffer is in transaction,
mark EOS when that buffer is returned.
This is observed in case client sends an empty buffer with EOS.

Change-Id: Idc050c9a76157bcf8a80e254e550f956537f98f1
Signed-off-by: Praveen Chavan <pchavan@codeaurora.org>

drivers/video/msm/vidc/common/vcd/vcd_sub.c
@@ -2359,8 +2359,16 @@ u32 vcd_handle_frame_done(
 
 	if (cctxt->decoding)
 		op_frm->vcd_frm.frame = transc->frame;
-	else
-		transc->frame = op_frm->vcd_frm.frame;
+	else {
+               transc->frame = op_frm->vcd_frm.frame;
+               if ((transc->flags & VCD_FRAME_FLAG_EOS) &&
+                       !(op_frm->vcd_frm.flags & VCD_FRAME_FLAG_EOS)) {
+                       op_frm->vcd_frm.flags |= VCD_FRAME_FLAG_EOS;
+                       pr_err("%s: add EOS flag to the output "\
+                               "from transc(0x%x)",
+                               __func__, (u32)transc);
+               }
+        }
 	transc->frame_done = true;
 
 	if (transc->input_done && transc->frame_done) {

CyanogenMod/android_kernel_lge_v500

drivers/video/msm/vidc/common/vcd/vcd_sub.c
@@ -2368,8 +2368,16 @@ u32 vcd_handle_frame_done(
 
 	if (cctxt->decoding)
 		op_frm->vcd_frm.frame = transc->frame;
-	else
+	else {
 		transc->frame = op_frm->vcd_frm.frame;
+		if ((transc->flags & VCD_FRAME_FLAG_EOS) &&
+			!(op_frm->vcd_frm.flags & VCD_FRAME_FLAG_EOS)) {
+			op_frm->vcd_frm.flags |= VCD_FRAME_FLAG_EOS;
+			VCD_MSG_HIGH("%s: add EOS flag to the output "\
+				"from transc(0x%x)",
+				__func__, (u32)transc);
+		}
+	}
 	transc->frame_done = true;
 
 	if (transc->input_done && transc->frame_done) {