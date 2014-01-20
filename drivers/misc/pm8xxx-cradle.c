CyanogenMod/android_kernel_lge_msm8974

BU52031NVX: Add lid events to support the smartcover

drivers/misc/pm8xxx-cradle.c
@@ -6,6 +6,7 @@
 
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/input.h>
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/interrupt.h>
@@ -44,6 +45,8 @@ struct pm8xxx_cradle {
 
 static struct workqueue_struct *cradle_wq;
 static struct pm8xxx_cradle *cradle;
+
+static struct input_dev  *cradle_input;
 
 static void boot_cradle_det_func(void)
 {
@@ -86,6 +89,11 @@ static void boot_cradle_det_func(void)
 	cradle->state = state;
 	wake_lock_timeout(&cradle->wake_lock, msecs_to_jiffies(3000));
 	switch_set_state(&cradle->sdev, cradle->state);
+
+        input_report_switch(cradle_input, SW_LID, 
+                cradle->state == SMARTCOVER_POUCH_OPENED ? 0 : 1);
+        input_sync(cradle_input);
+
 }
 
 #if defined CONFIG_MACH_MSM8974_VU3_KR
@@ -203,6 +211,9 @@ static void pm8xxx_pouch_work_func(struct work_struct *work)
 		wake_lock_timeout(&cradle->wake_lock, msecs_to_jiffies(3000));
 		switch_set_state(&cradle->sdev, cradle->state);
 		printk("%s : [Cradle] pouch value is %d\n", __func__ , state);
+                input_report_switch(cradle_input, SW_LID, 
+                        cradle->state == SMARTCOVER_POUCH_OPENED ? 0 : 1);
+                input_sync(cradle_input);
 	}
 	else {
 		spin_unlock_irqrestore(&cradle->lock, flags);
@@ -585,10 +596,40 @@ static struct platform_driver pm8xxx_cradle_driver = {
 	},
 };
 
+static int cradle_input_device_create(void){
+        int err = 0;
+
+        cradle_input = input_allocate_device();
+        if (!cradle_input) {
+                err = -ENOMEM;
+                goto exit;
+        }
+
+        cradle_input->name = "smartcover";
+        cradle_input->phys = "/dev/input/smartcover";
+
+        set_bit(EV_SW, cradle_input->evbit);
+        set_bit(SW_LID, cradle_input->swbit);
+
+        err = input_register_device(cradle_input);
+        if (err) {
+                goto exit_free;
+        }
+        return 0;
+
+exit_free:
+        input_free_device(cradle_input);
+        cradle_input = NULL;
+exit:
+        return err;
+
+}
+
 static int __init pm8xxx_cradle_init(void)
 {
-	cradle_wq = create_singlethread_workqueue("cradle_wq");
-       printk(KERN_ERR "cradle init \n");
+        cradle_input_device_create();
+        cradle_wq = create_singlethread_workqueue("cradle_wq");
+        printk(KERN_ERR "cradle init \n");
 	if (!cradle_wq)
 		return -ENOMEM;
 	return platform_driver_register(&pm8xxx_cradle_driver);
@@ -599,6 +640,7 @@ static void __exit pm8xxx_cradle_exit(void)
 {
 	if (cradle_wq)
 		destroy_workqueue(cradle_wq);
+        input_unregister_device(cradle_input);
 	platform_driver_unregister(&pm8xxx_cradle_driver);
 }
 module_exit(pm8xxx_cradle_exit);

CyanogenMod/android_kernel_lge_v500

drivers/misc/pm8xxx-cradle.c
@@ -20,6 +20,7 @@
  */
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/input.h>
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/interrupt.h>
@@ -58,6 +59,8 @@ void cradle_install_notify_handler(cradle_notify_handler handler)
     }
 }
 
+static struct input_dev  *cradle_input;
+
 static void boot_cradle_det_func(void)
 {
 	int state;
@@ -75,6 +78,11 @@ static void boot_cradle_det_func(void)
 	pr_info("%s : [Cradle] boot cradle value is %d\n", __func__ , state);
 	cradle->state = state;
 	switch_set_state(&cradle->sdev, cradle->state);
+
+	input_report_switch(cradle_input, SW_LID, 
+			cradle->state == CRADLE_SMARTCOVER_NO_DEV ? 0 : 1);
+	input_sync(cradle_input);
+
 }
 
 static void pm8xxx_cradle_work_func(struct work_struct *work)
@@ -96,6 +104,10 @@ static void pm8xxx_cradle_work_func(struct work_struct *work)
 
 	pr_info("%s : [Cradle] cradle value is %d\n", __func__ , state);
 	cradle->state = state;
+	input_report_switch(cradle_input, SW_LID, 
+			cradle->state == CRADLE_SMARTCOVER_NO_DEV ? 0 : 1);
+	input_sync(cradle_input);
+
 	spin_unlock_irqrestore(&cradle->lock, flags);
 
 	wake_lock_timeout(&cradle->wake_lock, msecs_to_jiffies(3000));
@@ -310,8 +322,38 @@ static struct platform_driver pm8xxx_cradle_driver = {
 	},
 };
 
+static int cradle_input_device_create(void){
+	int err = 0;
+
+	cradle_input = input_allocate_device();
+	if (!cradle_input) {
+		err = -ENOMEM;
+		goto exit;
+	}
+
+	cradle_input->name = "smartcover";
+	cradle_input->phys = "/dev/input/smartcover";
+
+	set_bit(EV_SW, cradle_input->evbit);
+	set_bit(SW_LID, cradle_input->swbit);
+
+	err = input_register_device(cradle_input);
+	if (err) {
+		goto exit_free;
+	}
+	return 0;
+
+exit_free:
+	input_free_device(cradle_input);
+	cradle_input = NULL;
+exit:
+	return err;
+
+}
+
 static int __init pm8xxx_cradle_init(void)
 {
+	cradle_input_device_create();
 	cradle_wq = create_singlethread_workqueue("cradle_wq");
 	pr_err("%s: cradle init \n", __func__);
 	if (!cradle_wq)
@@ -325,6 +367,7 @@ static void __exit pm8xxx_cradle_exit(void)
 {
 	if (cradle_wq)
 		destroy_workqueue(cradle_wq);
+	input_unregister_device(cradle_input);
 	platform_driver_unregister(&pm8xxx_cradle_driver);
 }
 module_exit(pm8xxx_cradle_exit);