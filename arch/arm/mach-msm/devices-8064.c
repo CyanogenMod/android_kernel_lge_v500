83df07ade79b9c33bced2975bd93b7018933ed0b
syphyr <syphyr@gmail.com>
2016-07-16 19:14:18 +0200

    Revert "msm: video: awifi: Restrict dpb count for smoothstreaming mode"
    This reverts commit fe19d3a5d20a7485d56353eaa46222ae215cece9.

--- arch/arm/mach-msm/devices-8064.c
+++ arch/arm/mach-msm/devices-8064.c
@@ -1779,11 +1779,7 @@ struct msm_vidc_platform_data apq8064_vidc_platform_data = {
 #endif
 	.disable_dmx = 0,
 	.disable_fullhd = 0,
-#ifdef CONFIG_MACH_APQ8064_AWIFI
-	.cont_mode_dpb_count = 14,
-#else
 	.cont_mode_dpb_count = 18,
-#endif
 	.fw_addr = 0x9fe00000,
 	.enable_sec_metadata = 0,
 };
